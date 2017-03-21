364fdc394f27f82eea8604bcaa405b88aad9b431
sophiaxhe <sophiahxx@gmail.com>
2017-03-21 15:43:49 -0400

    added category test

--- lib/package.rb
+++ lib/package.rb
@@ -1,11 +1,29 @@
 class Package
-  attr_accessor :base_price
+  attr_accessor :base_price, :base_markup
 
-  def initialize(options=>{})
+  def initialize(options={})
     self.base_price = options[:base_price]
   end
 
-  def base_markup
+  def add_base_markup
     (self.base_price * 1.05).round(2)
   end
+
+  def add_category(*args)
+    args.each { |c|
+    if c == "Food"
+      return (self.base_markup * 1.13).round(2)
+    end
+    if c == "Pharmacuticals"
+      return (self.base_markup * 1.075).round(2)
+    end
+    if c == "Electronics"
+      return (self.base_markup * 1.02).round(2)
+    end
+    if c == "Others"
+      return (self.base_markup).round(2)
+    end
+    }
+  end
+
 end
--- spec/lib/package_spec.rb
+++ spec/lib/package_spec.rb
@@ -3,9 +3,36 @@ require "package"
 
 
 describe Package do
-  it "has base markup" do
+  before (:each) do
     @package = Package.new
     @package.base_price = 1299.99
-    expect(@package.base_markup).to eq(1364.99)
+    @package.base_markup = 1364.99
   end
+  # it "has base markup" do
+  #   @package = Package.new
+  #   @package.base_price = 1299.99
+  #   expect(@package.base_markup).to eq(1364.99)
+  # end
+
+  context "category" do
+    it "adds 13% markup to food" do
+      expect(@package.category("Food")).to eq(1542.44)
+    end
+
+    it "adds 7.5% markup for pharmacuticals" do
+      # @package.base_markup = 1364.99
+      expect(@package.category("Pharmacuticals")).to eq(1467.36)
+    end
+
+    it "adds 2% markup for electronics" do
+      # @package.base_markup = 1364.99
+      expect(@package.category("Electronics")).to eq(1392.29)
+    end
+
+    it "doesn't add markup for Others category" do
+      # @package.base_markup = 1364.99
+      expect(@package.category("Others")).to eq(1364.99)
+    end
+  end
+
 end
